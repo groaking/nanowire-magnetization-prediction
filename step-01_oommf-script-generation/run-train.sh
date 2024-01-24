cb89c934fa6974f1d50bc551a9ccb6c43e1927ae
groaking <93555329+groaking@users.noreply.github.com>
2024-01-24 15:50:44 +0700

    Added the shell scripts

---
+++ step-01_oommf-script-generation/run-train.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Solve the OOMMF simulation problems from each generated MIF file
+
+# The oommf program
+alias oommf20='tclsh /home/longbowman/.oommf20/oommf.tcl'
+
+# The directory where all the MIF files are stored
+mif_dir="/ssynthesia/ghostcity/git/codename-cyborg/02312.00001_data-generation/generated-mifs"
+
+# The directory where the output ODT files will be exported to
+out_dir="/ssynthesia/ghostcity/git/codename-cyborg/02312.00002_simulation-run/odt-archives"
+
+# Iterate through every MIF file and simulate every one of them
+ls $mif_dir | sort -V | while read -r line; do oommf20 Boxsi -cwd $mif_dir -outdir $out_dir -- $line; done
